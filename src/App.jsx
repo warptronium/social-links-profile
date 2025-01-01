0a0066f8bddf71565af092d9891164ad96ef68fd
warptronium <lucasseg31@gmail.com>
2025-01-01 21:28:01 +0100

    initialisation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <body>
+        <p className="title">Hello</p>
+      </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
