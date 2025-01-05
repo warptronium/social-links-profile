ef4f1013a1623c6027781d5de48aaf6840f3afe0
warptronium <lucasseg31@gmail.com>
2025-01-05 22:22:04 +0100

    ajout de l'avatar et fix div box

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import "./App.css"
 import ButtonLink from "./components/ButtonLink/ButtonLink";
+import imageAvatar from './assets/images/image_avatar.jpg';
 
 function App() {
   return (
     <header className="App-header">
       <body>
         <div className="box">
-          <div className="nameProfile">
-            <p className="title">hello</p>
+          <div className="profile">
+            <img src={imageAvatar} className="imageAvatar" alt="image_avatar" />
+            <p className="title">warptronium</p>
           </div>
           <div className="buttons-links">
             <ButtonLink title="Youtube"/>
             <ButtonLink title="X"/>
             <ButtonLink title="Twitch"/>
+            <ButtonLink title="Tiktok"/>
+            <ButtonLink title="Instagram"/>
           </div>
         </div>
       </body>
